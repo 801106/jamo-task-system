d6509922881458e00011868cdfd536411aa61b24
801106 <presest@hotmail.com>
2026-04-28 11:25:34 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,27 +1,2 @@
-import { NextResponse } from 'next/server'
-
-export async function middleware(req) {
-  const { pathname } = req.nextUrl
-
-  // Publiczne - bez logowania
-  if (pathname === '/' || 
-      pathname === '/privacy' || 
-      pathname === '/eula' ||
-      pathname.startsWith('/api/') ||
-      pathname.startsWith('/_next/')) {
-    return NextResponse.next()
-  }
-
-  // Sprawdź token Supabase w cookies
-  const token = req.cookies.get('sb-rewjvzuuhbqeacexpmwv-auth-token')
-  
-  if (!token) {
-    return NextResponse.redirect(new URL('/', req.url))
-  }
-
-  return NextResponse.next()
-}
-
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
-}
+export function middleware() {}
+export const config = { matcher: [] }
